c8cce53956b1376f8cc5a76d4ff74f36cd48e3c1
unknown <juanmiscj25@gmail.com>
2021-01-31 13:57:00 -0500

    Particles and movement function (Not animated yet)

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path")
+
+http.createServer(function(req, res) {
+
+    if (req.url == "/") {
+        res.writeHead(200, {"content-type":"text/html;charset=UTF-8"});
+        res.end(fs.readFileSync("./index.html"));
+    }
+    else if (req.url.match(/.css$/)) {
+        res.writeHead(200, {"content-type":"text/css;charset=UTF-8"});
+        res.end(fs.readFileSync(path.join(__dirname, "public", req.url)));
+    }
+    else if (req.url.match(/.js$/)) {
+        res.writeHead(200, {"content-type":"text/js;charset=UTF-8"});
+        res.end(fs.readFileSync(path.join(__dirname, "public", req.url)));
+    }
+    else if (req.url.match(/png$/)) {
+        res.writeHead(200, {"content-type":"image/png"});
+        res.end(fs.readFileSync(path.join(__dirname, "public", req.url)));
+    }
+    else {
+        res.writeHead(404, {"content-type":"text/plain"});
+        res.end("ERROR 404. No se reconoce el archivo especificado.;")
+    }
+
+}).listen(3000);
+
+console.log("Servidor funcionando...")
